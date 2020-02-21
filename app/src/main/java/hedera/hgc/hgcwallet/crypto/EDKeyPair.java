2fd80163e45471d2cc692024c94b2e42e1974883
Sree <skaimal@opencrowd.com>
2020-02-21 16:18:26 -0500

    LicenseHeaderFixes

--- app/src/main/java/hedera/hgc/hgcwallet/crypto/EDKeyPair.java
+++ app/src/main/java/hedera/hgc/hgcwallet/crypto/EDKeyPair.java
@@ -1,20 +1,9 @@
-/*
- *
- *  Copyright 2019 Hedera Hashgraph LLC
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
+/**
+* Parts of this code is adopted from the below repository
+* src/main/java/com/ripple/crypto/ecdsa/EDKeyPair.java
+* at github repo
+* https://github.com/ripple-unmaintained/ripple-lib-java/blob/master/LICENSE
+*/
 
 package hedera.hgc.hgcwallet.crypto;
 
