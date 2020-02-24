bce19500dcde2ca33ef59e97bb7c84404d832e88
Sree <skaimal@opencrowd.com>
2020-02-24 16:46:18 -0500

    LicenseAttributionFix

--- app/src/main/java/hedera/hgc/hgcwallet/crypto/bip39/Mnemonic.java
+++ app/src/main/java/hedera/hgc/hgcwallet/crypto/bip39/Mnemonic.java
@@ -1,19 +1,9 @@
 /*
- * Copyright 2013 Ken Sedgwick
- * Copyright 2014 Andreas Schildbach
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* There are methods in this codebase that uses references from a few
+* Apache License 2.0 attribution repository below is the lists
+* https://github.com/bitcoinj/bitcoinj
+* core/src/main/java/org/bitcoinj/crypto/MnemonicCode.java
+*/
 
 package hedera.hgc.hgcwallet.crypto.bip39;
 
--- app/src/main/java/hedera/hgc/hgcwallet/ui/auth/fingerprint/FingerprintActivity.java
+++ app/src/main/java/hedera/hgc/hgcwallet/ui/auth/fingerprint/FingerprintActivity.java
@@ -1,20 +1,11 @@
-/*
- *
- *  Copyright 2019 Hedera Hashgraph LLC
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
+/**
+*
+* This code is an adaptation of Android OpenSource project codebase
+* Attributed to Apache 2.0 License
+* https://github.com/googlearchive/android-FingerprintDialog
+* Application/src/main/java/com/example/android/fingerprintdialog/MainActivity.java
+*
+*/
 
 package hedera.hgc.hgcwallet.ui.auth.fingerprint;
 
